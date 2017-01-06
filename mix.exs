c3f6b30911e2c3cc3c7ab6c734cc37a9458e94a9
elixir1.3 <foo@bar.com>
2017-01-06 14:38:50 +0000

    adding archives and build task

--- mix.exs
+++ mix.exs
@@ -2,11 +2,12 @@ defmodule Jsonstruct.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :jsonstruct,
-     version: "0.1.0",
+    [app: app(),
+     version: version(),
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     aliases: aliases,
      deps: deps()]
   end
 
@@ -17,6 +18,10 @@ defmodule Jsonstruct.Mixfile do
     [applications: [:logger,:exconstructor]]
   end
 
+  defp version, do: "0.0.2"
+
+  defp app, do: :jsonstruct
+
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
@@ -26,6 +31,23 @@ defmodule Jsonstruct.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
+
+  defp aliases do
+    [
+      build: [ &build_releases/1],
+    ]
+  end
+
+
+  
+  defp build_releases(_) do
+    Mix.Tasks.Compile.run([])
+    Mix.Tasks.Archive.Build.run([])
+    Mix.Tasks.Archive.Build.run(["--output=#{app}.ez"])
+    File.rename("#{app}.ez", "./archives/#{app}.ez")
+    File.rename("#{app}-#{version}.ez", "./archives/#{app}-#{version}.ez")
+  end
+
   defp deps do
     [
       {:poison, "~> 3.0"},
